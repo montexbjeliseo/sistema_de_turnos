6f445920fac82ba5e1081c162415e86dae6c6406
montexbjeliseo <secondmtx@gmail.com>
2023-05-20 12:19:12 -0300

    Tarea 2

--- sistema_de_turnos.py
+++ sistema_de_turnos.py
@@ -16,3 +16,21 @@ def solicitar_datos():
     }
 # Prueba Tarea 1 
 # print(solicitar_datos())
+
+# Tarea 2
+# Crear un menú con las siguientes opciones:
+# "Ingresar turno"
+# "Salir del Sistema"
+
+def menu():
+    """
+    Imprime el menú del sistema de turnos
+    """
+    print("*"*40)
+    print("Sistema de turnos")
+    print("*"*40)
+    print("1. Ingresar turno")
+    print("2. Salir del sistema")
+
+# Pruba Tarea 2    
+# menu()
